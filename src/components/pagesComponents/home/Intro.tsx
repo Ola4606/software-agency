78a2773ae7a4426794fee62bac7b0a84fbf57f4f
Ola4606 <olaoluwaadeyemo7@gmail.com>
2022-10-30 20:55:05 +0100

    changed about text

--- src/components/pagesComponents/home/Intro.tsx
+++ src/components/pagesComponents/home/Intro.tsx
@@ -2,7 +2,10 @@ import React from "react";
 
 function Intro(props: any) {
   return (
-    <div className="flex flex-col justify-start items-start px-4 mt-16 md:px-10 md:mt-24 lg:pl-40 lg:max-w-5xl lg:mt-40" ref={props.refp}>
+    <div
+      className="flex flex-col justify-start items-start px-4 mt-16 md:px-10 md:mt-24 lg:pl-40 lg:max-w-5xl lg:mt-40"
+      ref={props.refp}
+    >
       <p className="text-start text-black font-light font-titleNorm text-lg mb-2 md:text-xl md:mb-3 lg:text-2xl lg:mb-4">
         One-Stop Solution For All Your Business Needs
       </p>
@@ -11,10 +14,13 @@ function Intro(props: any) {
         Excellence
       </p>
       <p className="text-start text-black font-light font-titleNorm mb-10 text-lg md:mb-16 md:text-xl lg:text-2xl lg:mb-24">
-        A short About. A short About. A short About. A short About. A short
-        About.A short About. A short About. A short About. A short About. A
-        short About. A short About. A short About. A short About. A short About.
-        A short About. A short About. A short About. A short About.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+        turpis felis, laoreet non pellentesque eu, convallis eu elit. Curabitur
+        eleifend, sapien a bibendum imperdiet, purus ante fringilla nunc, tempus
+        ullamcorper turpis turpis sit amet mauris. Donec ullamcorper nunc ut
+        ligula mollis dapibus. Nullam ut augue rhoncus, elementum diam et,
+        blandit felis. Nulla id tortor id massa vehicula fringilla a vel mi. Sed
+        mollis efficitur aliquet.
       </p>
       <div className="bg-yellow-500 rounded-full text-black py-2 px-5 md:py-3 md:px-7 lg:py-7 lg:px-14 lg:cursor-pointer lg:transform lg:duration-300 lg:ease-in-out lg:hover:scale-105 lg:hover:text-white lg:hover:bg-indigo-500">
         <span className="text-center font-medium font-titleNorm text-base lg:text-lg">
